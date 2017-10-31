ccfb97bdd6b4af9b6fa0c3e30292bdb778e5beb0
diego llanos <diegojj1205@hotmail.com>
2017-10-30 20:10:43 -0400

    55

--- Listas_Circular_Estatica_Consola/Listas_Circular_Estatica_Consola/Aplicacion.h
+++ Listas_Circular_Estatica_Consola/Listas_Circular_Estatica_Consola/Aplicacion.h
@@ -1,12 +1,10 @@
 #pragma once
 #include "Memoria.h"
-#include "Monomio.h"
-#include "Lista.h"
-#include "Persona.h"
+#include "Lista_Circular.h"
 template<class tipo>
-class Aplicacion :public Memoria<tipo>, public Lista<tipo>
+class Aplicacion :public Memoria<tipo>, public Lista_Circular<tipo>
 {
-	Lista<Monomio> A,B;
+	
 
 
 
@@ -22,7 +20,7 @@ public:
 		}
 
 	}
-	Memoria<tipo> Lista_Memoria(Lista<tipo> ar)
+	Memoria<tipo> Lista_Memoria(Lista_Circular<tipo> ar)
 	{
 		Memoria<tipo> aux;
 		Nodo<tipo> uno;
@@ -30,7 +28,7 @@ public:
 		while (!ar.ListaVacia())
 		{
 
-			ar.Extraer(uno, -1);
+			ar.Eliminar(uno, -1);
 			aux.Arreglo(uno, i);
 			i++;
 		}
@@ -83,299 +81,6 @@ public:
 		else { return false; }
 	}
 
-	void  Intercambiar(int p, int q)
-	{
-		tipo dato;
-		Nodo<tipo> nodo2, nodo;
-
-		dato = TLista(p).Elemento();
-		nodo2 = TLista(p);
-		nodo2.Elemento(TLista(q).Elemento());
-		TLista(p, nodo2);
-		nodo = TLista(q);
-		nodo.Elemento(dato);
-		TLista(q, nodo);
-
-	}
-	void Ordenar()
-	{
-		int p, q, fin;
-		fin = this->Fin_Lista();
-		p = this->Primero();
-		while (p != fin)
-		{
-			q = this->Proximo(p);
-			while (q != fin)
-			{
-				if (TLista(p).Elemento()>TLista(q).Elemento())
-				{
-					Intercambiar(p, q);
-				}
-				q = Proximo(q);
-			}
-			p = Proximo(p);
-		}
-
-	}
-	void Eliminar_repetidos()
-	{
-		int p, q, fin, r;
-		Nodo<tipo> nodo, nodo1;
-		fin = this->Fin_Lista();
-		p = this->Primero();
-		while (p != fin)
-		{
-			nodo = this->TLista(p);
-			q = this->Proximo(p);
-			while (q != fin)
-			{
-				nodo1 = this->TLista(q);
-				if (nodo.Elemento() == nodo1.Elemento())
-				{
-					r = this->Anterior(q);
-					this->Extraer(nodo1, this->Anterior(q));
-					q = r;
-
-				}
-				q = this->Proximo(q);
-			}
-			p = this->Proximo(p);
-		}
-
-	}
-	void IngresarOrdenadamente(Nodo<tipo> x)
-	{
-
-		int p = this->Primero();
-		int f = this->Fin_Lista();
-		if (this->ListaVacia())
-		{
-			this->Insertar(x, -1);
-
-		}
-		else
-		{
-			while (p != f)
-			{
-				if (this->TLista(p).Elemento() > x.Elemento())
-				{
-
-					if (p == this->Primero())
-					{
-						this->Insertar(x, -1); break;
-					}
-
-					else { this->Insertar(x, this->Anterior(p)); break; }
-
-				}
-				else if (p == this->Ultimo())
-				{
-					this->Insertar(x, p); break;
-				}
-				p = this->Proximo(p);
-			}
-
-		}
-	}
-	void ClearList()
-	{
-		Nodo<tipo> aux;
-		while (!this->ListaVacia())
-		{
-			this->Extraer(aux, -1);
-		}
-		this->Tamano(0);
-	}
-	void Memoria_Polinomio(Memoria<tipo> x, char var)
-	{
-		
-		Nodo<Monomio> aux;
-		Monomio _part;
-		for (int i = 0;i < x.Tamano();i = i + 2)
-		{
-			_part.Coeficiente(x.Arreglo(i).Elemento());
-			_part.Exponente(x.Arreglo(i+1).Elemento());
-			aux.Elemento(_part);
-			if (var == 'A') 
-			{
-				A.Insertar(aux, -1);
-			}
-			else{ B.Insertar(aux, -1); }
-		}
-	}
-	Memoria<tipo> Polinomio_Memoria(Lista<Monomio>x)
-	{
-		Memoria<tipo> aux;
-		Nodo<Monomio> var;
-		Nodo<tipo> var2;
-		int i = 0;
-		while (!x.ListaVacia())
-		{
-			x.Extraer(var, -1);
-			var2.Elemento(var.Elemento().Coeficiente());
-			aux.Arreglo(var2, i);
-			i++;
-			var2.Elemento(var.Elemento().Exponente());
-			aux.Arreglo(var2, i);
-			i++;
-		}
-		aux.Tamano(i);
-		return aux;
-	}
-	Lista<tipo> EliminarSyN()
-	{
-		Lista<tipo>aux;
-		Nodo<tipo> var,var2;
-		int fin = this->Fin_Lista(),r;
-		int p = this->Primero();
-		while (p != fin)
-		{
-			var = this->TLista(p);
-			if (ValidoParaSN(var.Elemento()))
-			{
-				r = this->Anterior(p);
-				this->Extraer(var2, this->Localizar_Elemento_anterior(var));
-				aux.Insertar(var2, -1);
-				p = r;
-			}
-			p = this->Proximo(p);
-		}
-		return aux;
-	}
-	Lista<Monomio> This_Polinomio(char x)
-	{
-		if (x == 'A')
-		{
-			return A;
-		}
-		else { return B; }
-	}
-	void SumarPolinomio()
-	{
-		//el primero en salir deberia ser el mayor;
-		int primA=A.Primero();
-		int primB=B.Primero();
-		int finA = A.Fin_Lista();
-		int finB = B.Fin_Lista();
-		Nodo<Monomio> aux,x;
-		Monomio var;
-		while (primA != -1)
-		{
-			aux = A.TLista(primA);
-			while (primB != B.Fin_Lista())
-			{
-				if (aux.Elemento().Exponente() == B.TLista(primB).Elemento().Exponente())
-				{
-					var.Coeficiente(aux.Elemento().Coeficiente() + B.TLista(primB).Elemento().Coeficiente());
-					var.Exponente(aux.Elemento().Exponente());
-					x.Elemento(var);
-					B.TLista(primB, x); break;
-				}
-				else if (aux.Elemento().Exponente() >B.TLista(primB).Elemento().Exponente())
-				{
-					if (primB == B.Primero())
-					{
-						B.Insertar(aux, -1); break;
-					}
-					else
-					{
-						B.Insertar(aux, B.Anterior(primB)); break;
-					}
-					break;
-				}
-				
-				primB = B.Proximo(primB);
-			}
-	
-			primB = B.Primero();
-			finB = B.Fin_Lista();
-			primA = A.Proximo(primA);
-		}
-	}
-	Nodo<Persona> LaCosaDeManolo(int pos,int varus)// por editar
-	{
-		int pasos;
-		int p;
-		Nodo<Persona> aux;
-		if (pos == -1)
-		{
-			pasos = this->TLista(this->Primero()).Elemento().Numero();
-			p = this->Primero();
-		}
-		else
-		{
-			pasos= pos-1;
-			p = varus;
-		}
-		while (pasos--)
-		{
-			if (p == this->Ultimo())
-			{
-				p = this->Primero();
-			}
-			else
-			{
-				p = this->Proximo(p);
-			}
-		}
-
-
-		if (p == this->Primero())
-		{
-			this->Extraer(aux,-1);
-
-		}
-		else
-		{
-			this->Extraer(aux, this->Anterior(p));
-
-		}
-
-		if (p == this->Ultimo())
-		{
-			p = this->Primero();
-		}
-		else
-		{
-			p = this->Proximo(p);
-		}
-		aux.Apuntador(p);
-		return aux;
-
-
-	}
-	void Memoria_Personas(Memoria<string> x)
-	{
-		Persona per;
-		Nodo<tipo> aux;
-		for (int i = 0;i < x.Tamano();i=i+2)
-		{
-			per.Nombre(x.Arreglo(i).Elemento());
-			per.Numero(atoi((x.Arreglo(i+1).Elemento()).c_str()));
-			aux.Elemento(per);
-			this->Insertar(aux, -1);
-
-		}
-	}
-	Memoria<string> Persona_Memoria(Lista<Persona> personas)
-	{
-		Memoria<string> aux;
-		Nodo<Persona> var;
-		Nodo<string> var2;
-		int i = 0;
-		while (!personas.ListaVacia())
-		{
-			personas.Extraer(var, -1);
-			var2.Elemento(var.Elemento().Nombre());
-			aux.Arreglo(var2, i);
-			var2.Elemento(to_string(var.Elemento().Numero()));
-			aux.Arreglo(var2, i+1);
-			i = i + 2;
-
-		}
-		aux.Tamano(i);
-		return aux;
-	}
 	~Aplicacion() {}
 };
 
--- Listas_Circular_Estatica_Consola/Listas_Circular_Estatica_Consola/Lista_Circular.h
+++ Listas_Circular_Estatica_Consola/Listas_Circular_Estatica_Consola/Lista_Circular.h
@@ -9,12 +9,12 @@ class Lista_Circular
 	int BuscarNodo()
 	{
 		int y = _disp;
-		_disp = _lista[_disp].Apuntador();
+		_disp = _Lista[_disp].Apuntador();
 		return y;
 	}
 	void EliminarNodo(int y)
 	{
-		_lista[y].Apuntador(_disp);
+		_Lista[y].Apuntador(_disp);
 		_disp = y;
 	}
 public:
@@ -23,85 +23,27 @@ public:
 		int i = 0;
 		while (i < N - 1)
 		{
-			_lista[i].Apuntador(i + 1);
+			_Lista[i].Apuntador(i + 1);
 			i++;
 		}
-		_lista[i].Apuntador(-1);
+		_Lista[i].Apuntador(-1);
 		_disp = 0;
 		_list = -1;
 	}
 
 	//principales
 
-	bool ListaLLena()
-	{
-		if (_disp == -1) { return true; }
-		else { return false; }
-	}
-	bool ListaVacia()
-	{
-		if (_list == -1) { return true; }
-		else { return false; }
-	}
-	bool Insertar(Nodo<tipo> val,int p)
-	{
-		if (ListaLLena()) { return false; }
-		else
-		{
-			y = BuscarNodo();//busca el primero disponible para entrar
-			_Lista[y] = x;
-			if (p == -1)
-			{
-				if (ListaVacia()) { _list = y; }//si no hay nada
-				p = _list;
-			
-				//termina en que el unico elemento apunta a si mismo
-			}
-			else
-			{
-				if (_list == p) { _list = y; }
-			
+	bool ListaLLena() const;
 
-			}
-			_Lista[y].Apuntador(_Lista[p].Apuntador());//busca a donde apunta el ultimo
-			_Lista[p].Apuntador(y);
-			return true;
-		}
-	}
-	int Primero(int a)
-	{
-		return _list[a].Apuntador();
-	}
-	bool Eliminar(Nodo<tipo>&x, int p)
-	{
-		int y;
-		if (ListaVacia()) { return false; }
-		else
-		{
-			if (p == -1||p==_list)
-			{
-				y = this->Primero();
-				x = _Lista[y];
-				if (y == _list) { _list = -1; }
-				else
-				{
-					_Lista[_list].Apuntador(_Lista[y].Apuntador());
-				}
-			}
-			else
-			{
-				y = _Lista[p].Apuntador();
-				x = _Lista[y];
-				if (y == _list) { _list = p; }
-				_Lista[p].Apuntador(_Lista[y].Apuntador());
-				
-			}
-			this->EliminarNodo(y);
-			return true;
-		}
-	}
+	bool ListaVacia();
+
+	bool Insertar(Nodo<tipo> x, int p);
+
+	int Primero();
+
+	bool Eliminar(Nodo<tipo>& x, int p);
 	int Proximo(int i) { return _lista[i].Apuntador(); }
-	int Ultimo()
+	int Ultimo() const
 	{
 		return _list;
 	}
@@ -160,5 +102,87 @@ public:
 		Nodo<tipo> aux1, aux2;
 		
 	}
+	void ThisL(Lista_Circular<tipo>x) { *this = x; }
+	Lista_Circular<tipo> ThisL() { return *this; }
 	~Lista_Circular(){}
 };
+
+template <class tipo>
+bool Lista_Circular<tipo>::ListaLLena() const
+{
+	if (_disp == -1) { return true; }
+	else { return false; }
+}
+
+template <class tipo>
+bool Lista_Circular<tipo>::ListaVacia()
+{
+	if (_list == -1) { return true; }
+	else { return false; }
+}
+
+template <class tipo>
+bool Lista_Circular<tipo>::Insertar(Nodo<tipo> x, int p)
+{
+	int y;
+	if (ListaLLena()) { return false; }
+	else
+	{
+		y = BuscarNodo();//busca el primero disponible para entrar
+		_Lista[y] = x;
+		if (p == -1)
+		{
+			
+			if (ListaVacia())
+			{
+				_list = y;
+				_Lista[y].Apuntador(y);
+			}//si no hay nada
+			p = _list;
+
+			//termina en que el unico elemento apunta a si mismo
+		}
+		else
+		{
+			if (_list == p) { _list = y; }
+		}
+		_Lista[y].Apuntador(_Lista[p].Apuntador());//busca a donde apunta el ultimo
+		_Lista[p].Apuntador(y);
+		return true;
+	}
+}
+
+template <class tipo>
+int Lista_Circular<tipo>::Primero()
+{
+	return _Lista[_list].Apuntador();
+}
+
+template <class tipo>
+bool Lista_Circular<tipo>::Eliminar(Nodo<tipo>& x, int p)
+{
+	int y;
+	if (ListaVacia()) { return false; }
+	else
+	{
+		if (p == -1 || p == _list)
+		{
+			y = this->Primero();
+			x = _Lista[y];
+			if (y == _list) { _list = -1; }
+			else
+			{
+				_Lista[_list].Apuntador(_Lista[y].Apuntador());
+			}
+		}
+		else
+		{
+			y = _Lista[p].Apuntador();
+			x = _Lista[y];
+			if (y == _list) { _list = p; }
+			_Lista[p].Apuntador(_Lista[y].Apuntador());
+		}
+		this->EliminarNodo(y);
+		return true;
+	}
+}
